35c12d8d8e1cca3bbfcbf66d41648f8f65502515
Rios <lehuy.career@gmail.com>
2022-01-28 22:16:52 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hello Bé 🖐🖐🖐',
+    introDesc: `Một chút rảnh rỗi của chú á`,
+    btnIntro: 'Dạaaaaa',
+    title: 'Bé thấy anh là người như nàooo',
+    desc: 'Trả lời hoặc bị cốc đầu nha =)))) ',
+    btnYes: 'Để em kể cho ngheee',
+    btnNo: ' Khùng=)))) ',
+    question:'Giao thừa này em có rảnh khongggg ?',
+    reasonPlaceholder: 'Đón giao thừa cùng anh nhé',
+    btnReply: 'Gửi',
+    reply: '...',
+    mess: 'Quay lại messenger điiii',
+    messDesc: 'Anh ở bên đó á=)))',
+    btnAccept: 'Vâng ạaaaaa',
+    messLink: 'https://m.me/r.huy.38'
+}
